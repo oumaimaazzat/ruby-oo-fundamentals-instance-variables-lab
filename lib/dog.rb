f5454d4e4859a59330f63ceb9d77ff9fb96d84d7
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-20 12:18:12 -0500

    commit tree 0180d5b9ac41db737c0ae1e249b0479a005f9059

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,14 @@
 class Dog
-
-    def name=(dogs_name)
-      @this_dogs_name = dogs_name
-    end
-  
-    def name
-      @this_dogs_name
-    end
+  def name=(dog_name)
+    @this_dogs_name = dog_name
   end
-  
-  lassie = Dog.new
-  lassie.name = "Lassie"
-  
-  puts lassie.name
+
+  def name
+    @this_dogs_name
+  end
+end 
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
